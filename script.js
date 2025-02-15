51f8a1702aae924c9432ce95d109c9eb2d7c6747
anonymousProgrammer53 <aprogrammer53@gmail.com>
2025-02-15 18:58:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector('#hamburger-menu').addEventListener('click', () => {
+document.querySelector('#menu-icon').addEventListener('click', () => {
     document.querySelector('.navbar .navbar-nav').classList.toggle('active');
 });
 
 document.addEventListener('click', (e) => {
-    if (!document.querySelector('#hamburger-menu').contains(e.target) && !document.querySelector('.navbar > .navbar-nav').contains(e.target)) {
+    if (!document.querySelector('#menu-icon').contains(e.target) && !document.querySelector('.navbar .navbar-nav').contains(e.target)) {
         document.querySelector('.navbar .navbar-nav').classList.remove('active');
     }
 });
