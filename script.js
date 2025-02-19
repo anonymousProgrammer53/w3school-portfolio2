63876decbdfed0c0d56d2a96bb5425b235de7dcf
anonymousProgrammer53 <aprogrammer53@gmail.com>
2025-02-19 09:23:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.querySelector('#menu-icon').addEventListener('click', () => {
     document.querySelector('.navbar .navbar-nav').classList.toggle('active');
+    if (document.querySelector('#search-form').classList.contains('active')) {
+        document.querySelector('#search-form').classList.remove('active');
+    }
 });
 
 document.addEventListener('click', (e) => {
     if (!document.querySelector('#menu-icon').contains(e.target) && !document.querySelector('.navbar .navbar-nav').contains(e.target)) {
         document.querySelector('.navbar .navbar-nav').classList.remove('active');
     }
+});
+
+document.querySelector('#search').addEventListener('click', () => {
+    document.querySelector('#search-form').classList.toggle('active');
 });
